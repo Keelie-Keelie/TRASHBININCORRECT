e6d2f5d4ee26d4f1cb7b10d440fa36eed51819a0
Keelie-Keelie <78715386+Keelie-Keelie@users.noreply.github.com>
2021-07-08 12:17:12 -0700

    Add files via upload

--- paper.js
+++ paper.js
@@ -16,12 +16,12 @@ class Paper{
         World.add(world, this.body);
       }
       display(){
-       
+       var angle = this.body.angle;
         push();
         translate(this.body.position.x, this.body.position.y);
-        
+        rotate(angle);
         imageMode(CENTER);
-        image(this.image, 0, 0, this.r);
+        image(this.image, 0, 0, this.r, this.r);
         pop();
       }
 }
